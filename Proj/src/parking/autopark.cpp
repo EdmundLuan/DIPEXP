59d1f92caf5c8ddcaa0481edffb1e7063efb61b2
Edmund <55971071+EdmundLuan@users.noreply.github.com>
2019-12-07 13:19:42 +0800

    Started

--- Proj/src/parking/autopark.cpp
+++ Proj/src/parking/autopark.cpp
@@ -16,9 +16,53 @@
 #include "sensor_msgs/Image.h"
 #define LINEAR_X 0
 
+using namespace std;
 using namespace cv;
 
+static void onChange(int, void*) {
+    ;
+}
 
+void clrSeg(const Mat& src, Mat& des) {
+    int minH = 10, minS = 0, minV = 221;
+    int maxH = 180, maxS = 50, maxV = 255;
+    //createTrackbar("minH", "src", &minH, 179, onChange, 0);
+    inRange(src, Scalar(minH, minS, minV), Scalar(maxH, maxS, maxV), des);
+}
+
+void morphOpt(const Mat& src, Mat& des) {
+    const Mat SE = getStructuringElement(MORPH_RECT, Size(5, 5));	// Structuring element
+// Open operation
+    morphologyEx(src, des, MORPH_OPEN, SE);
+// Close operation
+    morphologyEx(des, des, MORPH_CLOSE, SE);
+}
+
+void dtmPrkMrk(const Mat& src, Mat& mrk) {
+    Mat edges;
+//	Canny Edge Detection
+    Canny(src, edges, 100, 200, 5, false);
+//	imshow("edges", edges);
+
+// Find contours
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    Mat cntrPic = Mat::zeros(edges.rows, edges.cols, CV_8UC3);
+
+    findContours(edges, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+    for(int i = 0; i < contours.size(); i++)
+        drawContours(cntrPic, contours, i, Scalar(255, 255, 255), 1);
+//	imshow("contours", cntrPic);
+
+//	Find Rectangles
+    vector<Rect> polyCntrs(contours.size());
+    for(int i = 0; i < contours.size(); i++) {
+        polyCntrs[i] = boundingRect(contours[i] );
+    }
+    for(int i = 0; i < contours.size(); i++)
+        rectangle(cntrPic, polyCntrs[i], Scalar(0, 0, 255));
+    imshow("contours", cntrPic);
+}
 
 
 /****************************** MAIN **************************************/
@@ -49,11 +93,26 @@ int main(int argc, char **argv) {
 // Mat frIn = frame();//使用笔记本摄像头
 //        Mat frIn = frame(cv::Rect(0, 0, frame.cols / 2, frame.rows));//截取 zed 的左目图片
         Mat frIn = frame.clone();
-		imshow("Original", frIn);
-		Mat frHSV;
-		cvtColor(frIn, frHSV, COLOR_BGR2HSV);	// Convert to HSV
-		imshow("HSV", frHSV);
 
+// Gaussian Filter
+        GaussianBlur(frIn, frIn, Size(3, 3), 1);
+        imshow("RGB", frIn);
+
+// Convert to HSV
+        Mat frHSV;
+        cvtColor(frIn, frHSV, COLOR_BGR2HSV);
+        imshow("HSV", frHSV);
+
+// Color Segmentation
+        Mat prkMrk;
+        clrSeg(frHSV, prkMrk);
+        imshow("White", prkMrk);
+
+// Morphological operations
+        morphOpt(prkMrk, prkMrk);
+
+// Parking mark detection, determination and  fining
+        dtmPrkMrk(prkMrk, prkMrk);
 
 
         ros::spinOnce();
@@ -61,4 +120,3 @@ int main(int argc, char **argv) {
     }
     return 0;
 }
-
